7db35a174da24fc94c31dc298374d935ef589637
mucahittozcu <tozcumucahit@gmail.com>
2024-02-22 16:09:12 +0300

    Wikipedia Makalesi

---
+++ src/utilities/GetArticles.js
@@ -0,0 +1,27 @@
+import articlesData from "@/data/ArticlesData"
+
+
+
+export default function getArticles(num, defNum = 4) {
+    const maxNum = articlesData.length
+    const articleArray = []
+    const tempArray = new Array(num || defNum).fill("").forEach(getRandomArticle)
+    
+    function getRandomArticle() {
+      const randomArticle = articlesData[Math.floor(Math.random() * maxNum)]
+      let duplicateDetected = false
+      articleArray.forEach(article => {
+        if (article.id === randomArticle.id){
+          duplicateDetected = true
+          }  })
+        if (duplicateDetected) {
+          getRandomArticle()
+        } else {
+          articleArray.push(randomArticle)
+        }
+    }
+    
+    return articleArray
+    
+  }
+  
